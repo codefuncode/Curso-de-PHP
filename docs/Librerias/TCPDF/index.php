a714bef8f8c0709304ca0bc1247c8bf43a734ef7
Carlos Aleman <caleman9791@gmail.com>
2022-02-20 20:39:41 -0400

    ok

---
+++ docs/Librerias/TCPDF/index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <ul>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_002.php" target="_blank">
+          002
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_003.php" target="_blank">
+          003
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_004.php" target="_blank">
+          004
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_005.php" target="_blank">
+          005
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_006.php" target="_blank">
+          006
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_007.php" target="_blank">
+          007
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_008.php" target="_blank">
+          008
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_009.php" target="_blank">
+          009
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_010.php" target="_blank">
+          010
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_011.php" target="_blank">
+          011
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_012.php" target="_blank">
+          012
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_013.php" target="_blank">
+          013
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_014.php" target="_blank">
+          014
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_015.php" target="_blank">
+          015
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_016.php" target="_blank">
+          016
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_017.php" target="_blank">
+          017
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_018.php" target="_blank">
+          018
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_019.php" target="_blank">
+          019
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_020.php" target="_blank">
+          020
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_021.php" target="_blank">
+          021
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_022.php" target="_blank">
+          022
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_023.php" target="_blank">
+          023
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_024.php" target="_blank">
+          024
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_025.php" target="_blank">
+          025
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_026.php" target="_blank">
+          026
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_027.php" target="_blank">
+          027
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_028.php" target="_blank">
+          028
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_029.php" target="_blank">
+          029
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_030.php" target="_blank">
+          030
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_031.php" target="_blank">
+          031
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_032.php" target="_blank">
+          032
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_033.php" target="_blank">
+          033
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_034.php" target="_blank">
+          034
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_035.php" target="_blank">
+          035
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_036.php" target="_blank">
+          036
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_037.php" target="_blank">
+          037
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_038.php" target="_blank">
+          038
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_039.php" target="_blank">
+          039
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_040.php" target="_blank">
+          040
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_041.php" target="_blank">
+          041
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_042.php" target="_blank">
+          042
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_043.php" target="_blank">
+          043
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_044.php" target="_blank">
+          044
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_045.php" target="_blank">
+          045
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+      <li>
+        <a href="./TCPDF-main/examples/example_001.php" target="_blank">
+          001
+        </a>
+      </li>
+    </ul>
+  </body>
+</html>
