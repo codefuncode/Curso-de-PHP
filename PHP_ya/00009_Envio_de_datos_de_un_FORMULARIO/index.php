ecfb12ed6451475f94edae688a6d270db991b409
Carlos Aleman <caleman9791@gmail.com>
2022-04-19 20:20:12 -0400

    Envío de formulario

---
+++ PHP_ya/00009_Envio_de_datos_de_un_FORMULARIO/index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>
+      Formulario de entrada del dato
+    </title>
+  </head>
+  <body>
+    <form action="index2.php" method="post">
+      Ingrese su nombre:
+      <input name="nombre" type="text"/>
+      <br/>
+      <input type="submit" value="confirmar"/>
+    </form>
+  </body>
+</html>
