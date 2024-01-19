5857911e30c9a107a533f0892477a1b094de0707
Carlos Alemán <caleman9791@gmail.com>
2024-01-18 22:53:26 -0400

    Revisión de la documentación  etiquetas php

---
+++ devdoc/00001-Sintaxis-basica/00001-Etiquetas-de-PHP/php.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>
+			Documento
+		</title>
+	</head>
+	<body>
+		<h1>
+			PHP Language
+		</h1>
+		<!-- <?php echo "1) Código de sintaxis especial PHP"; ?><br> -->
+
+		<?="2) Código de sintaxis especial PHP";?><br>
+
+		 <!-- Not Working -->
+		<? "3) Código de sintaxis especial PHP" ?>
+
+		<?php /* COMMENT */// ... más código /* COMMENT */ // el script finaliza aquí sin etiqueta de cierre de PHP   ?>
+	</body>
+</html>
