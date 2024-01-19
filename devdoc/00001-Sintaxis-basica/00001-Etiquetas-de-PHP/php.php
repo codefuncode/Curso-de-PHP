98195e2b91220c8ff7b7285d8f1e0d1c1af6afdb
Carlos Alemán <caleman9791@gmail.com>
2024-01-18 22:55:28 -0400

    Fichero php con el  ejemplo

--- devdoc/00001-Sintaxis-basica/00001-Etiquetas-de-PHP/php.php
+++ devdoc/00001-Sintaxis-basica/00001-Etiquetas-de-PHP/php.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<title>
-			Documento
-		</title>
-	</head>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP</title>
+</head>
 	<body>
 		<h1>
-			PHP Language
+			Lenguaje PHP
 		</h1>
+		<!--  Los comentarios con HTML no surgen efecto con php embebido  -->
 		<!-- <?php echo "1) Código de sintaxis especial PHP"; ?><br> -->
 
 		<?="2) Código de sintaxis especial PHP";?><br>
